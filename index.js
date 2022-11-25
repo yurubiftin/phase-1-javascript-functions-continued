2912b68d5f31ed810fb185afd3ac791877c465a6
yurubahmed <yurubiftin2002@gmail.com>
2022-11-25 16:15:18 -0500

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+//let activity = "roller-skate"
+function saturdayFun (activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+function  mondayWork(activity="go to the office"){
+return  `This Monday, I will ${activity}.`
+}
+function wrapAdjective (symbol ="*" ){
+    const emphatic = function (adjective = "special"){
+        return `You are ${symbol}${adjective}${symbol}!`
+    };
+    return emphatic ;}
